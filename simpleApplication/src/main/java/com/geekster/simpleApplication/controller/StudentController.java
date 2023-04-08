cdf0c7c7128fdd0c4eea3e8c1bbd128a5fb8a6e6
Anuvab123 <ghoshanuvab2000@gmail.com>
2023-04-09 01:55:57 +0530

    commit done

--- simpleApplication/src/main/java/com/geekster/simpleApplication/controller/StudentController.java
+++ simpleApplication/src/main/java/com/geekster/simpleApplication/controller/StudentController.java
@@ -1,6 +1,9 @@
 package com.geekster.simpleApplication.controller;
 
+import com.geekster.simpleApplication.model.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +11,13 @@ public class StudentController {
     @GetMapping(value = "/student")
     public String getStudent(){
         return "This is the information about student";
+    }
+
+    @PostMapping(value="/student")
+    public String saveStudent(@RequestBody Student studentObj){
+        System.out.println(studentObj.getStudentId());
+        System.out.println(studentObj.getFirstName());
+        System.out.println(studentObj.getLastName());
+        return "student data saved";
     }
 }
