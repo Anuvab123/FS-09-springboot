2c055fec7bddd79a4e0ff3714d569cbba7fd99b4
Anuvab123 <ghoshanuvab2000@gmail.com>
2023-04-09 00:09:14 +0530

    modified

--- simpleApplication/src/main/java/com/geekster/simpleApplication/controller/CatController.java
+++ simpleApplication/src/main/java/com/geekster/simpleApplication/controller/CatController.java
@@ -1,5 +1,7 @@
 package com.geekster.simpleApplication.controller;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +17,11 @@ public class CatController {
 
     @PostMapping(value="/cat")
     public String saveCat(@RequestBody String requestData){
-        System.out.println(requestData);
+        JSONObject jsonObject=new JSONObject(requestData);
+        String name=jsonObject.getString("name");
+        String username=jsonObject.getString("username");
+        String phno=jsonObject.getString("phn-no");
+        System.out.println(name+" "+username+" "+phno);
         return "data saved";
     }
 
